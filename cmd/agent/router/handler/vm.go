4a63f0e7934187db589ddee8cc2a7771affccceb
aaron <462826@qq.com>
2021-07-06 10:49:47 +0800

    testing

--- cmd/agent/router/handler/vm.go
+++ cmd/agent/router/handler/vm.go
@@ -2,9 +2,9 @@ package handler
 
 import (
 	"fmt"
-	kvmService "github.com/easysoft/zagent/internal/agent/service/kvm"
-	commDomain "github.com/easysoft/zagent/internal/comm/domain"
-	_domain "github.com/easysoft/zagent/internal/pkg/domain"
+	"github.com/easysoft/zagent/internal/agent/service/kvm"
+	"github.com/easysoft/zagent/internal/comm/domain"
+	"github.com/easysoft/zagent/internal/pkg/domain"
 	"golang.org/x/net/context"
 )
 
@@ -17,28 +17,29 @@ func NewVmCtrl() *VmCtrl {
 	return &VmCtrl{}
 }
 
-func (c *VmCtrl) Create(ctx context.Context, req commDomain.KvmReq, reply *_domain.RpcResp) error {
+func (c *VmCtrl) Create(ctx context.Context, req domain.KvmReq, reply *_domain.RpcResp) error {
 	dom, vncPort, vmRawPath, vmBackingPath, err := c.LibvirtService.CreateVm(&req)
 	if err == nil {
 		reply.Success("success to create vm.")
+
+		vmName, _ := dom.GetName()
+		vm := domain.Vm{
+			Name:        vmName,
+			VncPort:     vncPort,
+			ImagePath:   vmRawPath,
+			BackingPath: vmBackingPath,
+		}
+
+		reply.Payload = vm
+
 	} else {
 		reply.Fail(fmt.Sprintf("fail to create vm, error: %s", err.Error()))
 	}
 
-	vmName, _ := dom.GetName()
-	vm := commDomain.Vm{
-		Name:        vmName,
-		VncPort:     vncPort,
-		ImagePath:   vmRawPath,
-		BackingPath: vmBackingPath,
-	}
-
-	reply.Payload = vm
-
 	return nil
 }
 
-func (c *VmCtrl) Destroy(ctx context.Context, req commDomain.KvmReq, reply *_domain.RpcResp) error {
+func (c *VmCtrl) Destroy(ctx context.Context, req domain.KvmReq, reply *_domain.RpcResp) error {
 
 	return nil
 }
