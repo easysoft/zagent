407d3cd7fa46885d1b8d1f8d827bb2e223f54846
aaron <462826@qq.com>
2021-07-08 17:08:31 +0800

    testing on remote vm

--- internal/server/service/build.go
+++ internal/server/service/build.go
@@ -7,6 +7,7 @@ import (
 
 type BuildService struct {
 	BuildRepo *repo.BuildRepo `inject:""`
+	QueueRepo *repo.QueueRepo `inject:""`
 }
 
 func NewBuildService() *BuildService {
@@ -15,6 +16,9 @@ func NewBuildService() *BuildService {
 
 func (s BuildService) SaveResult(build domain.Build) (count int) {
 	s.BuildRepo.SaveResult(build)
+
+	po := s.BuildRepo.GetBuild(build.ID)
+	s.QueueRepo.SetQueueStatus(po.QueueId, build.Progress, build.Status)
 
 	return
 }
