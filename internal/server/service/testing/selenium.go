be5d4e692d69ad175666719f353dc81a3ed1c91c
aaron <462826@qq.com>
2021-07-12 13:49:35 +0800

    create histories

--- internal/server/service/testing/selenium.go
+++ internal/server/service/testing/selenium.go
@@ -1,6 +1,7 @@
 package testing
 
 import (
+	consts "github.com/easysoft/zagent/internal/comm/const"
 	_domain "github.com/easysoft/zagent/internal/pkg/domain"
 	"github.com/easysoft/zagent/internal/server/model"
 	"github.com/easysoft/zagent/internal/server/repo"
@@ -12,8 +13,9 @@ type SeleniumService struct {
 	BuildRepo *repo.BuildRepo `inject:""`
 	VmRepo    *repo.VmRepo    `inject:""`
 
-	RpcService   *commonService.RpcService   `inject:""`
-	QueueService *serverService.QueueService `inject:""`
+	RpcService     *commonService.RpcService     `inject:""`
+	QueueService   *serverService.QueueService   `inject:""`
+	HistoryService *serverService.HistoryService `inject:""`
 }
 
 func NewSeleniumService() *SeleniumService {
@@ -26,6 +28,7 @@ func (s SeleniumService) Start(queue model.Queue) (result _domain.RpcResp) {
 
 	build := model.NewSeleniumBuildPo(queue, vm)
 	s.BuildRepo.Save(&build)
+	s.HistoryService.Create(consts.Build, build.ID, consts.ProgressCreated, "")
 
 	build = s.BuildRepo.GetBuild(build.ID)
 
