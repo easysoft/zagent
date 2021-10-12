c356e39978468a2ad109be0e784dda4cb79292d9
aaron <462826@qq.com>
2021-10-12 11:11:43 +0800

    create vmware vm

--- cmd/test/vmware/api_test.go
+++ cmd/test/vmware/api_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestNewClient(t *testing.T) {
 	srv := vmwareService.NewVMWareService()
-	srv.Connect("https://127.0.0.1:8697", "aaron", "P@ssw0rd")
+	srv.Connect("https://127.0.0.1:8697", "aaron", "P2ssw0rd@")
 
 	vm, _ := srv.CreateVm("win10-pro-x64-zh_cn", "test-win10-pro-x64-zh_cn", 2, 1024*4)
 	log.Printf("%#v", vm)
