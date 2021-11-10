19c488508c547382d0cea3e5fc0b9214ffbfbf0f
aaron <462826@qq.com>
2021-11-10 14:58:51 +0800

    register

--- xdoc/start.bat
+++ xdoc/start.bat
@@ -5,6 +5,6 @@
 ping /n 3 114.114.114.114 | findstr "TTL=" && goto next || goto start
 
 :next
-cmd /k "cd /d C:\Users\jenkins\dev\project\zagent && git fetch --all && git reset --hard origin/main && git pull && go env -w GOPROXY=https://goproxy.io && go get all && go run cmd\agent-vm\main.go -t vm"
+cmd /k "cd /d C:\Users\admin\dev\project\zagent && git fetch --all && git reset --hard origin/main && git pull && go env -w GOPROXY=https://goproxy.io && go get all && go run cmd\agent-vm\main.go -t vm -s http://192.168.0.232:8455"
 
 pause
