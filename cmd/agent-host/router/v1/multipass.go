d1d324649d99899d83848e619743e9547bb593b4
hind3ight <1157595693@qq.com>
2022-01-07 12:42:52 +0800

    multipass manage vm

--- cmd/agent-host/router/v1/multipass.go
+++ cmd/agent-host/router/v1/multipass.go
@@ -1,27 +1,37 @@
 package v1
 
+//type MultiPassReq struct {
+//	VmId          string `json:"vmId"` // for destroy
+//	VmUniqueName  string `json:"vmName"`
+//	VmBackingName string `json:"VmBackingName"`
+//
+//	VmProcessors uint `json:"vmProcessors"`
+//	VmMemory     uint `json:"vmMemory"`
+//
+//	UserName string `json:"userName"`
+//	Password string `json:"password"`
+//
+//	StartAfterCreated bool `json:"startAfterCreated"`
+//}
 type MultiPassReq struct {
-	VmId          string `json:"vmId"` // for destroy
-	VmUniqueName  string `json:"vmName"`
-	VmBackingName string `json:"VmBackingName"`
-
-	VmProcessors uint `json:"vmProcessors"`
-	VmMemory     uint `json:"vmMemory"`
-
-	UserName string `json:"userName"`
-	Password string `json:"password"`
-
-	StartAfterCreated bool `json:"startAfterCreated"`
+	Name     string `json:"name"`
+	Memory   string `json:"memory"`
+	Cpus     string `json:"cpus"`
+	Disk     string `json:"disk"`
+	FilePath string `json:"vmRauPath"`
 }
 
 type MultiPassResp struct {
 	Code int `json:"code"`
 	Msg  int `json:"msg"`
 
-	VmId       string `json:"vmId"`
-	Name       string `json:"name"`
-	Mac        string `json:"mac"`
-	VncAddress string `json:"vncAddress"`
+	Name    string `json:"name"`
+	Cpus    string `json:"cpus"`
+	Memory  string `json:"memory"`
+	Network string `json:"network"`
+	State   string `json:"state"`
+	Image   string `json:"image"`
+	IPv4    string `json:"i_pv_4"`
 }
 
 type MultiPassParam struct {
