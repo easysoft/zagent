572dbd41a73a4c30e5282a95fb47d669d53cef14
hind3ight <1157595693@qq.com>
2022-01-07 12:42:52 +0800

    multipass manage vm

--- internal/comm/domain/multipass.go
+++ internal/comm/domain/multipass.go
@@ -1,8 +1,11 @@
 package domain
 
 type MultiPass struct {
-	Name  string `json:"name"`
-	State string `json:"state"`
-	Image string `json:"image"`
-	IPv4  string `json:"i_pv_4"`
+	Name    string `json:"name"`
+	Cpus    string `json:"cpus"`
+	Memory  string `json:"memory"`
+	Network string `json:"network"`
+	State   string `json:"state"`
+	Image   string `json:"image"`
+	IPv4    string `json:"i_pv_4"`
 }
