908e4c9f4afc3b5dbf1bb8225449eecc89864590
hind3ight <1157595693@qq.com>
2022-01-12 19:26:06 +0800

    multipass getVncToken

--- internal/comm/domain/multipass.go
+++ internal/comm/domain/multipass.go
@@ -1,11 +1,17 @@
 package domain
 
 type MultiPass struct {
-	Name    string `json:"name"`
-	Cpus    string `json:"cpus"`
-	Memory  string `json:"memory"`
-	Network string `json:"network"`
-	State   string `json:"state"`
-	Image   string `json:"image"`
-	IPv4    string `json:"i_pv_4"`
+	Name        string `json:"name"`
+	State       string `json:"state"`
+	IPv4        string `json:"ipv4"`
+	Release     string `json:"release"`
+	ImageHash   string `json:"imageHash"`
+	Load        string `json:"load"`
+	DiskUsage   string `json:"diskUsage"`
+	MemoryUsage string `json:"memoryUsage"`
+	Mounts      string `json:"mounts"`
+	Cpus        string `json:"cpus"`
+	Memory      string `json:"memory"`
+	Network     string `json:"network"`
+	Image       string `json:"image"`
 }
