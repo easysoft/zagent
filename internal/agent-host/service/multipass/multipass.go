aae75005ac12fc4ceee9da0bf4348d96d2d3a041
hind3ight <1157595693@qq.com>
2022-01-13 18:17:33 +0800

    optimize multipassService

--- internal/agent-host/service/multipass/multipass.go
+++ internal/agent-host/service/multipass/multipass.go
@@ -36,7 +36,92 @@ type MultiPassService struct {
 	syncMap sync.Map
 }
 
-func (s *MultiPassService) GetVms() (doms []domain.MultiPass, err error) {
+func (s *MultiPassService) CreateVm(name, filePath, imgFrom string, cpus, disk, mem uint) (dom domain.MultiPass, err error) {
+	if name != "" {
+		vm := s.GetVmInfo(name)
+		if vm.Name != "" {
+			msg := "vm %s has existed"
+			_logUtils.Errorf(msg, name)
+			err = errors.New(msg)
+			return
+		}
+		cmdMpLaunch = cmdMpLaunch + fmt.Sprintf("-n %s ", name)
+	}
+
+	if cpus >= 1 {
+		cmdMpLaunch = cmdMpLaunch + fmt.Sprintf("-c %v ", cpus)
+	}
+	if disk >= 0 {
+		cmdMpLaunch = cmdMpLaunch + fmt.Sprintf("-d %vG ", disk)
+	}
+	if mem >= 0 {
+		cmdMpLaunch = cmdMpLaunch + fmt.Sprintf("-m %vM ", mem)
+	}
+	if filePath != "" {
+		filePath = filepath.Join(agentConf.Inst.DirImage, filePath)
+		if imgFrom == "file" || imgFrom == "url" {
+			cmdMpLaunch = cmdMpLaunch + fmt.Sprintf("%s://%s", imgFrom, filePath)
+		}
+	}
+	_, err = _shellUtils.ExeShellWithOutput(cmdMpLaunch)
+	if s.GetVmInfo(name).State != "Running" && err != nil {
+		_logUtils.Errorf(err.Error())
+		return
+	}
+
+	return
+}
+
+func (s *MultiPassService) RebootVmByName(name string) (dom domain.MultiPass, err error) {
+	_, err = _shellUtils.ExeShellWithOutput(fmt.Sprintf(cmdMpReboot, name))
+	if s.GetVmInfo(name).State != "Running" && err != nil {
+		_logUtils.Errorf(err.Error())
+		return
+	}
+	return
+}
+
+func (s *MultiPassService) DestroyVm(name string) (dom domain.MultiPass, err error) {
+	_, err = _shellUtils.ExeShellWithOutput(fmt.Sprintf(cmdMpDelete+"&&"+cmdMpPurge, name))
+	if s.GetVmInfo(name).Name == "" && err != nil {
+		_logUtils.Errorf(err.Error())
+		return
+	}
+	return
+}
+
+func (s *MultiPassService) SuspendVmByName(name string) (dom domain.MultiPass, err error) {
+	_, err = _shellUtils.ExeShellWithOutput(fmt.Sprintf(cmdMpSuspend, name))
+	if s.GetVmInfo(name).State != "Suspended" && err != nil {
+		_logUtils.Errorf(err.Error())
+		return
+	}
+	return
+}
+
+func (s *MultiPassService) ResumeVmByName(name string) (dom domain.MultiPass, err error) {
+	_, err = _shellUtils.ExeShellWithOutput(fmt.Sprintf(cmdMpStart, name))
+	if s.GetVmInfo(name).State != "Running" && err != nil {
+		_logUtils.Errorf(err.Error())
+		return
+	}
+	return
+}
+
+func (s *MultiPassService) GetToken(port string) (ret v1.VncTokenResp) {
+	s.GenWebsockifyTokens()
+	obj, ok := s.syncMap.Load(mpTokenPrefix + port)
+
+	if !ok {
+		return
+	}
+
+	ret = obj.(v1.VncTokenResp)
+
+	return
+}
+
+func (s *MultiPassService) GetVmList() (doms []domain.MultiPass, err error) {
 	outRets, err := _shellUtils.ExeShellWithOutput(cmdMpls)
 	if err != nil {
 		_logUtils.Errorf(err.Error())
@@ -59,7 +144,7 @@ func (s *MultiPassService) GetVms() (doms []domain.MultiPass, err error) {
 	return
 }
 
-func (s *MultiPassService) VmInfo(name string) (dom domain.MultiPass) {
+func (s *MultiPassService) GetVmInfo(name string) (dom domain.MultiPass) {
 	outRets, err := _shellUtils.ExeShellWithOutput(fmt.Sprintf(cmdMpInfo, name))
 	if strings.Contains(outRets[0], "info failed:") || err != nil {
 		_logUtils.Errorf(err.Error())
@@ -68,9 +153,14 @@ func (s *MultiPassService) VmInfo(name string) (dom domain.MultiPass) {
 
 	vmInfoMap := parseOutput(outRets[:len(outRets)-1])
 	dom = domain.MultiPass{
-		Name:  vmInfoMap["Name"],
-		State: vmInfoMap["State"],
-		IPv4:  vmInfoMap["IPv4"],
+		Name:        vmInfoMap["Name"],
+		State:       vmInfoMap["State"],
+		IPv4:        vmInfoMap["IPv4"],
+		Release:     vmInfoMap["Release"],
+		Load:        vmInfoMap["load"],
+		DiskUsage:   vmInfoMap["diskUsage"],
+		MemoryUsage: vmInfoMap["memoryUsage"],
+		Mounts:      vmInfoMap["mounts"],
 	}
 
 	return
@@ -86,79 +176,8 @@ func parseOutput(lines []string) (vmInfoMap map[string]string) {
 	return
 }
 
-func (s *MultiPassService) CreateVm(name, cpus, disk, mem, filePath string) (dom domain.MultiPass, err error) {
-	if name != "" {
-		vm := s.VmInfo(name)
-		if vm.Name == "" {
-			msg := "vm %s not found"
-			_logUtils.Errorf(msg, name)
-			err = errors.New(msg)
-			return
-		}
-		cmdMpLaunch = cmdMpLaunch + fmt.Sprintf("-n %s ", name)
-	}
-
-	if cpus != "" {
-		cmdMpLaunch = cmdMpLaunch + fmt.Sprintf("-c %s ", cpus)
-	}
-	if disk != "" {
-		cmdMpLaunch = cmdMpLaunch + fmt.Sprintf("-d %s ", disk)
-	}
-	if mem != "" {
-		cmdMpLaunch = cmdMpLaunch + fmt.Sprintf("-m %s ", mem)
-	}
-	if filePath != "" {
-		filePath = filepath.Join(agentConf.Inst.DirImage, filePath)
-
-		cmdMpLaunch = cmdMpLaunch + fmt.Sprintf("file://%s ", filePath)
-	}
-	outRets, err := _shellUtils.ExeShellWithOutput(cmdMpLaunch)
-	if strings.Contains(outRets[0], "launch failed") == true || err != nil {
-		_logUtils.Errorf(err.Error())
-		return
-	}
-
-	return
-}
-
-func (s *MultiPassService) RebootVmByName(name string) (dom domain.MultiPass, err error) {
-	_, err = _shellUtils.ExeShellWithOutput(fmt.Sprintf(cmdMpReboot, name))
-	if s.VmInfo(name).State != "Running" && err != nil {
-		_logUtils.Errorf(err.Error())
-		return
-	}
-	return
-}
-
-func (s *MultiPassService) DestroyVm(name string) (dom domain.MultiPass, err error) {
-	_, err = _shellUtils.ExeShellWithOutput(fmt.Sprintf(cmdMpDelete+"&&"+cmdMpPurge, name))
-	if s.VmInfo(name).Name == "" && err != nil {
-		_logUtils.Errorf(err.Error())
-		return
-	}
-	return
-}
-
-func (s *MultiPassService) SuspendVmByName(name string) (dom domain.MultiPass, err error) {
-	_, err = _shellUtils.ExeShellWithOutput(fmt.Sprintf(cmdMpSuspend, name))
-	if s.VmInfo(name).State != "Suspended" && err != nil {
-		_logUtils.Errorf(err.Error())
-		return
-	}
-	return
-}
-
-func (s *MultiPassService) ResumeVmByName(name string) (dom domain.MultiPass, err error) {
-	_, err = _shellUtils.ExeShellWithOutput(fmt.Sprintf(cmdMpStart, name))
-	if s.VmInfo(name).State != "Running" && err != nil {
-		_logUtils.Errorf(err.Error())
-		return
-	}
-	return
-}
-
-func (s *MultiPassService) GenWebsockifyTokens() {
-	vms, _ := s.GetVms()
+func (s *MultiPassService) GenWebsockifyTokens() { // create tokenFile
+	vms, _ := s.GetVmList()
 	port := 5901
 	for _, v := range vms {
 		if v.State != "Running" {
@@ -166,7 +185,7 @@ func (s *MultiPassService) GenWebsockifyTokens() {
 		}
 		portStr := strconv.Itoa(port)
 
-		// uuid: 192.168.1.215:5901
+		// uuid: vmIp:5901
 		content := fmt.Sprintf("%s: %s:%s", _stringUtils.NewUuid(), agentConf.Inst.NodeIp, portStr)
 
 		pth := filepath.Join(agentConf.Inst.DirToken, mpTokenPrefix+portStr+".txt")
@@ -182,17 +201,4 @@ func (s *MultiPassService) GenWebsockifyTokens() {
 
 		port++
 	}
-}
-
-func (s *MultiPassService) GetToken(port string) (ret v1.VncTokenResp) {
-	s.GenWebsockifyTokens()
-	obj, ok := s.syncMap.Load(mpTokenPrefix + port)
-
-	if !ok {
-		return
-	}
-
-	ret = obj.(v1.VncTokenResp)
-
-	return
 }
