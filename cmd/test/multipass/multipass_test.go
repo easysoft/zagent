6c90e065683e9176d0279d7cdc92e9e7c2d5ff07
hind3ight <1157595693@qq.com>
2022-01-20 15:40:14 +0800

    multipass test

--- cmd/test/multipass/multipass_test.go
+++ cmd/test/multipass/multipass_test.go
@@ -3,18 +3,24 @@ package multipass
 import (
 	v1 "github.com/easysoft/zv/cmd/agent-host/router/v1"
 	multiPassService "github.com/easysoft/zv/internal/agent-host/service/multipass"
+	agentConf "github.com/easysoft/zv/internal/agent/conf"
+	consts "github.com/easysoft/zv/internal/comm/const"
+	_logUtils "github.com/easysoft/zv/internal/pkg/lib/log"
 	"log"
 	"testing"
 )
 
 func TestMultiPass(t *testing.T) {
+	_logUtils.Init(consts.AppNameAgentHost)
+	agentConf.Inst.DirImage = "/home/hind3ight/kvm/image/"
+
 	srv := multiPassService.MultiPassService{}
 	req := v1.MultiPassReq{
 		VmUniqueName: "test1",
 		VmMemory:     4096,
 		Disk:         10,
 		Cpus:         1,
-		FilePath:     "ubuntu.img",
+		ImagePath:    "ubuntu.img",
 		ImgFrom:      "file",
 	}
 
