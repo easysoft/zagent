76473e6d35226ada452550deea53d094d429e9fb
aaron <462826@qq.com>
2022-09-02 19:42:22 +0800

    add guide

--- cmd/test/_const/const.go
+++ cmd/test/_const/const.go
@@ -1,18 +1,18 @@
 package testconst
 
 const (
-	ALIYUN_KEY = "LTAI5t8sfNV3M5a7VWJfzGZ2"
-	ALIYUN_Secret = "Y8znXiZ5ZANjoyDjztR0bmm2xSfOfD"
-	ALIYUN_REGION    = "cn-hangzhou"
-	ALIYUN_VPC    = "vpc-bp1p8nzox2o3kvfh4o0wo"
-	ALIYUN_NAMEAPACE    = ""
+	ALIYUN_KEY       = ""
+	ALIYUN_Secret    = ""
+	ALIYUN_REGION    = ""
+	ALIYUN_VPC       = ""
+	ALIYUN_NAMEAPACE = ""
 
-	HUAWEI_CLOUD_KEY        = "TVZFYALVUODSL6S8FTNX"
-	HUAWEI_CLOUD_Secret     = "rF81YZEE9slggcqDBO5aBHtIh63mWi2g8saeUIe5"
-	HUAWEI_CLOUD_REGION     = "cn-east-3"
-	HUAWEI_CLOUD_NAMEAPACE    = "cci-tester"
+	HUAWEI_CLOUD_KEY       = ""
+	HUAWEI_CLOUD_Secret    = "TVZFYALVUODSL6S8FTNX"
+	HUAWEI_CLOUD_REGION    = ""
+	HUAWEI_CLOUD_NAMEAPACE = ""
 
-	HUAWEI_CLOUD_USER         = "aaronchen2k"
-	HUAWEI_CLOUD_IAM_USER     = "aaronchen2k-iam"
-	HUAWEI_CLOUD_IAM_PASSWORD = "z%T!cG\"q!vpCOJj!m4hI"
+	HUAWEI_CLOUD_USER         = "cci-tester"
+	HUAWEI_CLOUD_IAM_USER     = ""
+	HUAWEI_CLOUD_IAM_PASSWORD = ""
 )
