99684019d70c94c9f1fddb18006a2c2d8075f384
aaron <462826@qq.com>
2022-09-02 19:55:03 +0800

    add guide

---
+++ cmd/test/_const/const.go
@@ -0,0 +1,18 @@
+package testconst
+
+const (
+	ALIYUN_KEY       = ""
+	ALIYUN_Secret    = ""
+	ALIYUN_REGION    = ""
+	ALIYUN_VPC       = ""
+	ALIYUN_NAMEAPACE = ""
+
+	HUAWEI_CLOUD_KEY       = ""
+	HUAWEI_CLOUD_Secret    = ""
+	HUAWEI_CLOUD_REGION    = ""
+	HUAWEI_CLOUD_NAMEAPACE = ""
+
+	HUAWEI_CLOUD_USER         = ""
+	HUAWEI_CLOUD_IAM_USER     = ""
+	HUAWEI_CLOUD_IAM_PASSWORD = ""
+)
