862a5a6119af0a2cce4c76962c83edeca663461d
aaron <462826@qq.com>
2022-09-07 16:43:48 +0800

    create vbox vm

--- cmd/host/router/v1/virtualbox.go
+++ cmd/host/router/v1/virtualbox.go
@@ -4,13 +4,9 @@ type VirtualBoxReq struct {
 	VmUniqueName string `json:"vmUniqueName" example:"test-win10-x64-pro-zh_cn"`
 	BackingName  string `json:"backingName"`
 
-	Bridge       string `json:"bridge"`
-	VmCpu        uint   `json:"vmCpu"`
-	VmMemorySize uint   `json:"vmMemorySize"`
-	VncPort      int    `json:"vncPort"`
-
-	CloudIamUser     string `json:"cloudIamUser"`
-	CloudIamPassword string `json:"cloudIamPassword"`
+	VmCpu        uint `json:"vmCpu"`
+	VmMemorySize uint `json:"vmMemorySize"`
+	VncPort      int  `json:"vncPort"`
 
 	Prefix string `json:"prefix"`
 }
