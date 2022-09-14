624f9ad63f3c86e4579a05901e9ece341bce36ff
aaron <462826@qq.com>
2022-09-14 09:26:16 +0800

    update

--- internal/agent/utils/nat/nat.go
+++ internal/agent/utils/nat/nat.go
@@ -15,7 +15,7 @@ const (
 )
 
 func GetValidPort() (ret int, err error) {
-	cmd := fmt.Sprintf(`netstat -anp | awk '{print $7}' | grep -o '51[0-9]\{3\}'`)
+	cmd := fmt.Sprintf(`sudo iptables -t filter -L -n --line-number | awk '{print $1}' | grep -o '51[0-9]\{3\}'`)
 	output, err := _shellUtils.ExeSysCmd(cmd)
 	if err != nil {
 		return
@@ -36,12 +36,13 @@ func GetValidPort() (ret int, err error) {
 func ForwardPort(vmIp string, vmPort int, hostIp string, hostPort int) (err error) {
 	/**
 	sudo iptables -A INPUT -p tcp --dport 58086 -j ACCEPT
-	sudo iptables -t nat -A PREROUTING -d 192.168.0.56 -p tcp -m tcp --dport 58086 -j DNAT --to-destination 192.168.122.7:8086
-	sudo iptables -t nat -A POSTROUTING -s 192.168.122.0/255.255.255.0 -d 192.168.122.7 -p tcp -m tcp --dport 8086 -j SNAT --to-source 192.168.122.1
+	sudo iptables -t nat -A PREROUTING -d 10.0.7.248 -p tcp -m tcp --dport 58086 -j DNAT --to-destination 192.168.122.212:8086
+	sudo iptables -t nat -A POSTROUTING -s 192.168.122.0/255.255.255.0 -d 192.168.122.212 -p tcp -m tcp --dport 8086 -j SNAT --to-source 192.168.122.1
 
-	sudo iptables -nL -v --line-numbers -t filter
+	sudo iptables -t nat  -L -n --line-number
 	sudo iptables -D FORWARD 14 -t filter
 	sudo ufw disable && sudo ufw enable
+	sudo sysctl -a | grep forward
 	*/
 
 	cmd := fmt.Sprintf(`sudo iptables -A INPUT -p tcp --dport %d -j ACCEPT`, hostPort)
@@ -84,8 +85,8 @@ func RemoveForwardByVm(vmIp string, vmPort int, hostIp string, hostPort int) (er
 
 	_logUtils.Info(output)
 
-	// sudo iptables -t nat -L LIBVIRT_FWI -n --line-number | grep %s | awk '{print $1}'
-	cmd = fmt.Sprintf(`sudo iptables -t nat -L LIBVIRT_FWI -n --line-number | grep %s | awk '{print $1}'`, vmIp)
+	// sudo iptables -t nat  -L -n --line-number | grep 192.168.122.212 | awk '{print $1}'
+	cmd = fmt.Sprintf(`sudo iptables -t nat  -L -n --line-number | grep %s | awk '{print $1}'`, vmIp)
 	output, err = _shellUtils.ExeSysCmd(cmd)
 	if err != nil {
 		return
@@ -95,8 +96,11 @@ func RemoveForwardByVm(vmIp string, vmPort int, hostIp string, hostPort int) (er
 
 	for _, item := range arr {
 		// sudo iptables -nvL
-		// iptables -t nat -L LIBVIRT_FWI -D INPUT %s
-		cmd = fmt.Sprintf(`iptables -t nat -L LIBVIRT_FWI -D INPUT %s`, item)
+		// sudo iptables -t nat -D POSTROUTING 2
+		// sudo iptables -t filter -D LIBVIRT_FWI 2
+		// sudo iptables -t filter -D LIBVIRT_FWO 2
+
+		cmd = fmt.Sprintf(`iptables -t nat -D INPUT %s`, item)
 		output, err = _shellUtils.ExeSysCmd(cmd)
 	}
 
