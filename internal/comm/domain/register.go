bb88a015df65687329101429139260ec5861a05c
aaron <462826@qq.com>
2022-09-21 16:24:54 +0800

    testing

--- internal/comm/domain/register.go
+++ internal/comm/domain/register.go
@@ -12,7 +12,7 @@ type VmNotifyReq struct {
 type VmNotifyResp struct {
 	Secret          string `json:"secret"`
 	Ip              string `json:"ip"`
-	AgentPortOnHost int    `json:"ip"`
+	AgentPortOnHost int    `json:"agentPortOnHost"`
 }
 
 type RegisterResp struct {
