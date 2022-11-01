ea1ebb841f6783006aa99b206aa151b65e72b721
aaron <462826@qq.com>
2022-11-01 15:50:29 +0800

    testing check service status

--- internal/pkg/const/cmd.go
+++ internal/pkg/const/cmd.go
@@ -2,24 +2,24 @@ package consts
 
 const (
 	CmdCreateImage = `qemu-img create -f qcow2 \
-		-o cluster_size=2M,backing_file=%s \
-		%s 60G
-	`
+-o cluster_size=2M,backing_file=%s \
+%s 60G
+`
 
 	CmdCreateVm = `virt-install \
-		--connect qemu:///system \
-		--virt-type kvm \
-		--name %s \
-		--vcpus %d \
-		--ram %d \
-		--disk path=/home/aaron/zagent/kvm/image/test-1.qcow2,size=%d,sparse \
-		--network default \
-		--boot hd \
-		--os-variant generic \
-		--noreboot \
-		--wait -1 \
-		--force 
-	`
+--connect qemu:///system \
+--virt-type kvm \
+--name %s \
+--vcpus %d \
+--ram %d \
+--disk path=/home/aaron/zagent/kvm/image/test-1.qcow2,size=%d,sparse \
+--network default \
+--boot hd \
+--os-variant generic \
+--noreboot \
+--wait -1 \
+--force 
+`
 
 	CmdStartVm = `virsh --connect qemu:///system start %s`
 
