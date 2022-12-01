55d09d75c492a72ace26b6748df838a15b65e030
zhaoke <yuaiwuhen@hotmail.com>
2022-12-01 14:49:37 +0800

    * Fix issue

--- pkg/lib/shell/kill-win.go
+++ pkg/lib/shell/kill-win.go
@@ -6,6 +6,7 @@ package _shellUtils
 import (
 	"fmt"
 	"os/exec"
+	"regexp"
 	"strings"
 	"syscall"
 )
@@ -17,17 +18,18 @@ func KillProcessByUUID(uuid string) {
 	out, _ := cmd1.Output()
 	lines := strings.Split(string(out), "\n")
 	for index, line := range lines {
-		if index == 0 {
+		line = strings.TrimSpace(line)
+
+		reg := regexp.MustCompile(`( +)`)
+		line = reg.ReplaceAllString(line, " ")
+
+		if index == 0 || line == "" {
 			continue
 		}
-		line = strings.TrimSpace(line)
+
 		cols := strings.Split(line, " ")
-		pid := ""
-		if len(cols) > 3 {
-			pid = cols[3]
-		} else if len(cols) > 2 {
-			pid = cols[2]
-		}
+		pid := cols[len(cols)-1]
+
 		if pid != "" {
 			fmt.Println(fmt.Sprintf(`taskkill /F /pid %s`, pid))
 			cmd2 := exec.Command("cmd")
